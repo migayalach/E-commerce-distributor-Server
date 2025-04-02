b32cde0df997ee5450f4c307bd7f1940d0529121
migayalach <ayalachavezmiguel@gmail.com>
2025-04-02 14:33:51 -0400

    imports paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SubscribeModule } from './subscribe/subscribe.module';
+import { CategoryModule } from './category/category.module';
+import { ProductsModule } from './products/products.module';
+import { LevelModule } from './level/level.module';
+import { UserModule } from './user/user.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [SubscribeModule],
+  imports: [SubscribeModule, CategoryModule, ProductsModule, LevelModule, UserModule, FavoriteModule],
   controllers: [],
   providers: [],
 })
