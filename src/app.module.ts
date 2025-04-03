d332caa2002cd66cf442ed44e277bff85092f362
migayalach <ayalachavezmiguel@gmail.com>
2025-04-02 23:14:04 -0400

    initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { SubscribeModule } from './subscribe/subscribe.module';
 import { CategoryModule } from './category/category.module';
 import { ProductsModule } from './products/products.module';
 import { LevelModule } from './level/level.module';
 import { UserModule } from './user/user.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.DATABASE_LOCAL || 'mongodb://localhost/distributor',
+    ),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+    }),
     SubscribeModule,
     CategoryModule,
     ProductsModule,
