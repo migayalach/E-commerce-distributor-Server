bc81c448da65f6092a883293dd7d2d2de441121e
migayalach <ayalachavezmiguel@gmail.com>
2025-04-04 10:22:52 -0400

    resolver ready

--- src/subscribe/subscribe.resolver.ts
+++ src/subscribe/subscribe.resolver.ts
@@ -1,7 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { SubscribeService } from './subscribe.service';
+import { Subscribe } from './schema/subscribe.schema';
+import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 
 @Resolver()
 export class SubscribeResolver {
   constructor(private readonly subscribeService: SubscribeService) {}
+
+  @Query(() => [Subscribe])
+  async getSubscribe(): Promise<any> {
+    return await this.subscribeService.getAllSubs();
+  }
+
+  @Mutation(() => Subscribe)
+  async createSubscribe(
+    @Args('dataInput') dataInput: CreateSubscriptionDto,
+  ): Promise<any> {
+    return this.subscribeService.addSubEmail(dataInput);
+  }
 }
