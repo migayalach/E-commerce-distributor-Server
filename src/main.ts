2140d08e694602922e3d122658613e6068035ebb
migayalach <ayalachavezmiguel@gmail.com>
2025-04-04 23:16:21 -0400

    added new globalprefix with name 'distributor'

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('distributor');
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
