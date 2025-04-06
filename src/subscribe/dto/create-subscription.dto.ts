5246269ea43aed66aeeb1ee8cdb14ad314a2c045
migayalach <ayalachavezmiguel@gmail.com>
2025-04-05 23:43:39 -0400

    created dto

--- src/subscribe/dto/create-subscription.dto.ts
+++ src/subscribe/dto/create-subscription.dto.ts
@@ -1,7 +1,11 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateSubscriptionDto {
   @Field()
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty({ message: 'Please introduce a email' })
   email: string;
 }
