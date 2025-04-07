c9cb102421670969fbb25ce3f692b9cebabd9029
migayalach <ayalachavezmiguel@gmail.com>
2025-04-06 23:02:59 -0400

    import Mongoose and added Category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './schema/category.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+  ],
   providers: [CategoryResolver, CategoryService],
 })
 export class CategoryModule {}
