3f2f3bf45cf787fe0174dba5394f838d11dcc052
migayalach <ayalachavezmiguel@gmail.com>
2025-04-06 22:52:11 -0400

    added new function for clear data Category

--- helpers/clearData.helpers.ts
+++ helpers/clearData.helpers.ts
@@ -1,4 +1,5 @@
 import { DataSubs } from 'src/subscribe/interface/subscribe.interface';
+import { DataCategory } from 'src/category/interface/category.interface';
 import { parseIdObject } from './auxFunction.helpers';
 
 export const clearSubs = (data: DataSubs[]) => {
@@ -7,4 +8,26 @@ export const clearSubs = (data: DataSubs[]) => {
     email,
   }));
   return response;
+};
+
+export const clearCategory = (data: DataCategory[]) => {
+  const response = data.map(({ _id, nameCategory }) => ({
+    _id: parseIdObject(_id),
+    nameCategory,
+  }));
+  return response;
+};
+
+export const clearObjCategory = ({
+  _id,
+  nameCategory,
+}: {
+  _id: string;
+  nameCategory: string;
+}) => {
+  const obj = {
+    _id: parseIdObject(_id),
+    nameCategory,
+  };
+  return obj;
 };
