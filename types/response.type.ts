078295114097ed6e0a3c5b90fecdb8703c86fc4c
migayalach <ayalachavezmiguel@gmail.com>
2025-04-10 22:34:04 -0400

    added DataItem to colection

--- types/response.type.ts
+++ types/response.type.ts
@@ -1,5 +1,10 @@
 import { DataCategory } from 'src/category/interface/category.interface';
 import { DataLevel } from 'src/level/interface/level.interface';
+import { DataProduct } from 'src/products/interface/product.interface';
 import { DataSubs } from 'src/subscribe/interface/subscribe.interface';
 
-export type DataItem = DataSubs[] | DataCategory[] | DataLevel[];
+export type DataItem =
+  | DataSubs[]
+  | DataCategory[]
+  | DataLevel[]
+  | DataProduct[];
