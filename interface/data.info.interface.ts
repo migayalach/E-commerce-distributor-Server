1edfd20a3972cff87fb43d57c3815d8c326a1d6c
migayalach <ayalachavezmiguel@gmail.com>
2025-04-10 22:35:29 -0400

    added new interface for ResProduct

--- interface/data.info.interface.ts
+++ interface/data.info.interface.ts
@@ -1,5 +1,6 @@
 import { DataCategory } from 'src/category/interface/category.interface';
 import { DataLevel } from 'src/level/interface/level.interface';
+import { DataProduct } from 'src/products/interface/product.interface';
 import { DataSubs } from 'src/subscribe/interface/subscribe.interface';
 
 export interface RespInfoBase {
@@ -18,4 +19,8 @@ export interface ResCategory extends RespInfoBase {
 
 export interface ResLevel extends RespInfoBase {
   info: DataLevel;
+}
+
+export interface ResProduct extends RespInfoBase {
+  info: DataProduct;
 }
