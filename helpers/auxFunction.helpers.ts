d3eee2f9bf383818f07511e0825c35e810577f46
migayalach <ayalachavezmiguel@gmail.com>
2025-04-10 22:38:48 -0400

    added new funtion parseTypeIdMongo

--- helpers/auxFunction.helpers.ts
+++ helpers/auxFunction.helpers.ts
@@ -1,4 +1,13 @@
+import { Types } from 'mongoose';
+
 export const parseIdObject = (id: string): string => {
   const data = id.toString();
   return data;
+};
+
+export const parseTypeIdMongo = (id: string) => {
+  if (Types.ObjectId.isValid(id)) {
+    return new Types.ObjectId(id);
+  }
+  throw new Error('Invalid ObjectId');
 };
