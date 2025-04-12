17862f348d17bc32780a4d87331365621c860e64
migayalach <ayalachavezmiguel@gmail.com>
2025-04-12 01:20:40 -0400

    resolver complete and ready to use

--- src/password/password.resolver.ts
+++ src/password/password.resolver.ts
@@ -1,7 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { PasswordService } from './password.service';
+import { ChangePasswordDto } from './dto/chengePassword.dto';
+import { ResponseInfo } from '@interface/response.interface';
+import { DataPassword } from './interface/password.interface';
 
 @Resolver('Password')
 export class PasswordResolver {
   constructor(private readonly passwordService: PasswordService) {}
+
+  @Mutation(() => ResponseInfo)
+  async changePassword(
+    @Args('dataPassword') dataPassword: ChangePasswordDto,
+  ): Promise<DataPassword> {
+    return await this.passwordService.updatePassword(dataPassword);
+  }
 }
