40f15d08e3a433e6f7a77bbc6cbd1bc6115ccbd0
migayalach <ayalachavezmiguel@gmail.com>
2025-04-12 14:47:42 -0400

    import UserModule and Product model

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { FavoriteResolver } from './favorite.resolver';
+import { UserModule } from 'src/user/user.module';
+import { ProductModel } from 'src/products/models/product.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Favorite, FavoriteSchema } from './schema/favorite.schema';
 
 @Module({
+  imports: [
+    UserModule,
+    ProductModel,
+    MongooseModule.forFeature([
+      { name: Favorite.name, schema: FavoriteSchema },
+    ]),
+  ],
   providers: [FavoriteResolver, FavoriteService],
 })
 export class FavoriteModule {}
