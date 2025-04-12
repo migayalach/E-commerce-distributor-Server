be2aca9e517e04cf95297c663502e77501552ff4
migayalach <ayalachavezmiguel@gmail.com>
2025-04-11 22:34:00 -0400

    created schema for MONGO

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ type: Types.ObjectId, ref: 'Level', required: true })
+  idLevel: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  carnet: string;
+
+  @Prop({ required: true })
+  phone: number;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: false })
+  profilePicture: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
