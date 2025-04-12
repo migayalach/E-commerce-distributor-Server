a01d044b19cf23d0ad0287a56b6edf2f48366c69
migayalach <ayalachavezmiguel@gmail.com>
2025-04-12 14:48:17 -0400

    resolver favorite in proccess

--- src/favorite/favorite.resolver.ts
+++ src/favorite/favorite.resolver.ts
@@ -1,7 +1,26 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FavoriteService } from './favorite.service';
+import { CreateFavoriteDto } from './dto/createFavorite.dto';
+import { ResponseInfo } from '@interface/response.interface';
 
 @Resolver()
 export class FavoriteResolver {
   constructor(private readonly favoriteService: FavoriteService) {}
+
+  getAllIdFavorite() {}
+
+  @Mutation(() => ResponseInfo)
+  createFavorite(@Args('dataFavorite') dataFavorite: CreateFavoriteDto) {
+    return;
+  }
+
+  @Mutation(() => ResponseInfo)
+  removeFavorite(
+    @Args('idFavorite', { type: () => String, nullable: false })
+    idFavorite: string,
+    @Args('idProduct', { type: () => String, nullable: false })
+    idProduct: string,
+  ) {
+    return;
+  }
 }
