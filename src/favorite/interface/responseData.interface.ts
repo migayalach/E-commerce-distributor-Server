2a94eb572bae2015a1c3509fb51098d2c2f8743e
migayalach <ayalachavezmiguel@gmail.com>
2025-04-13 17:42:03 -0400

    delete files

--- src/favorite/interface/responseData.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ResponseInfo } from '@interface/response.interface';
-import { FavoriteModelGQL } from '@model/favorite.model';
-import { Field, ObjectType } from '@nestjs/graphql';
-
-@ObjectType()
-export class FavoriteResponse extends ResponseInfo {
-  @Field(() => FavoriteModelGQL)
-  info: FavoriteModelGQL;
-}
