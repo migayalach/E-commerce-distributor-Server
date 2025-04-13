2684ebae4bb6acdd9ae16e1ea7bb33bf1cd4349a
migayalach <ayalachavezmiguel@gmail.com>
2025-04-13 17:37:49 -0400

    import productModel

--- src/products/dto/pag-product-res.dto.ts
+++ src/products/dto/pag-product-res.dto.ts
@@ -1,6 +1,6 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 import { PaginatedResponse } from '@interface/response.interface';
-import { ProductModel } from '../models/product.model';
+import { ProductModel } from '@model/product.model';
 
 @ObjectType()
 export class PagProductResponse extends PaginatedResponse {
