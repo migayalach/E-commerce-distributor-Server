6dd05b653891e8250ede24b1957d16762bb4e2b3
migayalach <ayalachavezmiguel@gmail.com>
2025-04-13 17:17:07 -0400

    initial confinguration

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartResolver } from './cart.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cart, CartSchema } from './schema/cart.schema';
+import { UserModule } from 'src/user/user.module';
+import { ProductModel } from '@model/product.model';
 
 @Module({
+  imports: [
+    UserModule,
+    ProductModel,
+    MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
+  ],
+  exports: [CartService],
   providers: [CartResolver, CartService],
 })
 export class CartModule {}
---
+++ src/cart/interface/cart.interface.ts
@@ -0,0 +1,3 @@
+export interface DataCart {
+  _id: string;
+}
