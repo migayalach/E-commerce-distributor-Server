c1fa46f92766f76ee230a1dca4df7ca4f708af46
migayalach <ayalachavezmiguel@gmail.com>
2025-04-13 17:15:44 -0400

    added new class to consume at GQL as name UserIdModelGQL

--- models/user.model.ts
+++ models/user.model.ts
@@ -29,3 +29,39 @@ export class UserModelGQL {
   @Field()
   profilePicture: string;
 }
+
+@ObjectType()
+export class UserIdModelGQL {
+  @Field(() => ID)
+  _id: string;
+
+  @Field(() => ID)
+  idLevel: string;
+
+  @Field(() => ID)
+  idFavorite: string;
+
+  @Field(() => ID)
+  idCart: string;
+
+  @Field()
+  nameLevel: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  carnet: string;
+
+  @Field()
+  phone: number;
+
+  @Field()
+  profilePicture: string;
+}
