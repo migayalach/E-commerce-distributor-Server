813973f34bdecaa4a24d4ef2c19850b26f8b650c
migayalach <ayalachavezmiguel@gmail.com>
2025-04-14 17:41:57 -0400

    resolver buy ready to use

--- src/buy/buy.resolver.ts
+++ src/buy/buy.resolver.ts
@@ -1,7 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BuyService } from './buy.service';
+import { CreateBuyDto } from './dto/createBuy.dto';
+import { ResponseInfo } from '@interface/response.interface';
+import { PagBuyResponse } from './dto/pag-buy-res.dto';
+import { RespInfoBase } from '@interface/data.info.interface';
+import { Response } from '@interface/response.results.interface';
 
 @Resolver('Buy')
 export class BuyResolver {
   constructor(private readonly buyService: BuyService) {}
+
+  @Mutation(() => ResponseInfo)
+  async createBuy(
+    @Args('shoppingList') shoppingList: CreateBuyDto,
+  ): Promise<RespInfoBase> {
+    return await this.buyService.addBuy(shoppingList);
+  }
+
+  @Query(() => PagBuyResponse)
+  async getAllBuy(
+    @Args('idUser', { type: () => String, nullable: false }) idUser: string,
+    @Args('page', { type: () => Int, nullable: true }) page: number = 1,
+  ): Promise<Response> {
+    return this.buyService.getIdBuyUserAll(idUser, page);
+  }
 }
