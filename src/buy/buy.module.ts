c929af106eeb3da5774969a8f9ffdededcec74fb
migayalach <ayalachavezmiguel@gmail.com>
2025-04-14 17:41:29 -0400

    configuration ready

--- src/buy/buy.module.ts
+++ src/buy/buy.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { BuyService } from './buy.service';
 import { BuyResolver } from './buy.resolver';
-import { CartModule } from 'src/cart/cart.module';
+import { UserModule } from 'src/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Buy, BuySchema } from './schema/buy.schema';
+import { DetailModule } from 'src/detail/detail.module';
 
 @Module({
-  imports: [CartModule],
+  imports: [
+    UserModule,
+    DetailModule,
+    MongooseModule.forFeature([{ name: Buy.name, schema: BuySchema }]),
+  ],
+  exports: [BuyService],
   providers: [BuyResolver, BuyService],
 })
 export class BuyModule {}
