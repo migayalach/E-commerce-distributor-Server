f10cd7a8e1115b8a51602ec2eed2252890166fe9
migayalach <ayalachavezmiguel@gmail.com>
2025-04-13 20:18:32 -0400

    Resolver ready to use with next function getAll, Add or Delete products that cart and clear cart

--- src/cart/cart.resolver.ts
+++ src/cart/cart.resolver.ts
@@ -1,13 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CartService } from './cart.service';
+import { ActionCartDto } from './dto/actionCart.dto';
+import { ResponseInfo } from '@interface/response.interface';
+import { PagCartResponse } from './dto/pag-cart-res.dto';
+import { RespInfoBase } from '@interface/data.info.interface';
+import { Response } from '@interface/response.results.interface';
 
 @Resolver('Cart')
 export class CartResolver {
   constructor(private readonly cartService: CartService) {}
 
-  getAllIdCart() {}
+  @Query(() => PagCartResponse)
+  async getAllIdCart(
+    @Args('idCart', { type: () => String, nullable: false }) idCart: string,
+    @Args('page', { type: () => Int, nullable: true }) page: number = 1,
+  ): Promise<Response> {
+    return await this.cartService.getListCart(idCart, page);
+  }
 
-  actionCart() {}
+  @Mutation(() => ResponseInfo)
+  async actionCart(
+    @Args('dataCart') dataCart: ActionCartDto,
+  ): Promise<RespInfoBase> {
+    return await this.cartService.actionCart(dataCart);
+  }
 
-  clearCart() {}
+  @Mutation(() => ResponseInfo)
+  async clearCart(
+    @Args('idCart', { type: () => String, nullable: false }) idCart: string,
+  ): Promise<void> {
+    return await this.cartService.clearCart(idCart);
+  }
 }
