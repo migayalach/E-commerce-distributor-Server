f65a5beb90dc5d5925463ae98e4c0824c684c587
migayalach <ayalachavezmiguel@gmail.com>
2025-04-14 23:58:08 -0400

    alter schema added listProducts and created sub model for ProductDetail

--- src/cart/schema/cart.schema.ts
+++ src/cart/schema/cart.schema.ts
@@ -1,13 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
+@Schema({ _id: false })
+export class ProductDetail extends Document {
+  @Prop({ type: Types.ObjectId, ref: 'Product', required: true })
+  idProduct: string;
+
+  @Prop({ required: true })
+  amount: number;
+}
+
+export const ProductDetailSchema = SchemaFactory.createForClass(ProductDetail);
+
 @Schema()
 export class Cart extends Document {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   idUser: string;
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'Product' }], default: [] })
-  listProducts: Types.ObjectId[];
+  @Prop({ type: [ProductDetailSchema], default: [] })
+  listProducts: ProductDetail[];
 }
 
 export const CartSchema = SchemaFactory.createForClass(Cart);
