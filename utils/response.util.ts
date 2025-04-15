ae7c1ed2b04b193970598ba13dae0b2e34734c12
migayalach <ayalachavezmiguel@gmail.com>
2025-04-15 19:34:49 -0400

    created new function with name responseDetail

--- utils/response.util.ts
+++ utils/response.util.ts
@@ -1,5 +1,5 @@
-import { DataItem } from '../types/response.type';
-import { info } from './info.util';
+import { DataDatail, DataItem } from '../types/response.type';
+import { info, infoDetail } from './info.util';
 import { results } from './results.util';
 
 export const response = (data: DataItem, page: number) => {
@@ -17,6 +17,31 @@ export const response = (data: DataItem, page: number) => {
 
   return {
     info: info(data, page),
+    results: results(data, page),
+  };
+};
+
+export const responseDetail = (
+  data: DataDatail,
+  page: number,
+  totalBuy: number,
+) => {
+  if (!data.length) {
+    return {
+      info: {
+        count: 0,
+        pages: 0,
+        next: null,
+        prev: null,
+      },
+      totalBuy: 0,
+      results: [],
+    };
+  }
+
+  return {
+    info: infoDetail(data, page),
+    totalBuy,
     results: results(data, page),
   };
 };
