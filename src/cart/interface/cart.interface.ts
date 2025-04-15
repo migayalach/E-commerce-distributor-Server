1819c234bb12bfb7f60527aba0f8743312306be5
migayalach <ayalachavezmiguel@gmail.com>
2025-04-14 23:57:13 -0400

    created new interface for amount

--- src/cart/interface/cart.interface.ts
+++ src/cart/interface/cart.interface.ts
@@ -1,10 +1,26 @@
 import { Types } from 'mongoose';
+import { DataProductCart } from 'src/favorite/interface/favorite.interface';
+import { DataProduct } from 'src/products/interface/product.interface';
 
 export interface DataCart {
   _id: string;
 }
 
+export interface DataCartProduct {
+  idProduct: Types.ObjectId;
+  amount: number;
+}
+
 export interface CartUserList {
   _id: Types.ObjectId;
-  listProducts: Types.ObjectId[];
+  listProducts: DataCartProduct[];
+}
+
+export interface DataProductsCart extends DataProductCart {
+  amount: number;
+}
+
+export interface DataProductCartRes extends DataProduct {
+  _id: string;
+  amount: number;
 }
