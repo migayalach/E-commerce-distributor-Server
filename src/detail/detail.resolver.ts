feb6bb742124e343055172aa9e777cd3710cefd9
migayalach <ayalachavezmiguel@gmail.com>
2025-04-15 19:42:44 -0400

    resolver ready to use

--- src/detail/detail.resolver.ts
+++ src/detail/detail.resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { DetailService } from './detail.service';
+import { PagDetailResponse } from './dto/pag-detail-res.dto';
+import { ResponseDetail } from '@interface/response.results.interface';
 
 @Resolver('Detail')
 export class DetailResolver {
   constructor(private readonly detailService: DetailService) {}
 
-  getIdDetail() {}
+  @Query(() => PagDetailResponse)
+  async getIdDetail(
+    @Args('idBuy', { type: () => String, nullable: false }) idBuy: string,
+    @Args('page', { type: () => Int, nullable: true }) page: number = 1,
+  ): Promise<ResponseDetail> {
+    return await this.detailService.getIdDetail(idBuy, page);
+  }
 }
