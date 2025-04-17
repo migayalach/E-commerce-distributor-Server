4d8a9d412984732f7b51b8a427a89d4b4b0e372d
migayalach <ayalachavezmiguel@gmail.com>
2025-04-16 23:25:40 -0400

    seeders in process

---
+++ src/seeders/initialData.seeders.ts
@@ -0,0 +1,143 @@
+export const subscribe = [
+  { email: 'lucas.fernandez92@gmail.com' },
+  { email: 'marisol.rojas87@gmail.com' },
+  { email: 'andres.tec.dev@gmail.com' },
+  { email: 'camila_estrella21@gmail.com' },
+  { email: 'juanma.codes@gmail.com' },
+  { email: 'natalia.writer99@gmail.com' },
+  { email: 'leo.desingart@gmail.com' },
+  { email: 'sofia.devweb@gmail.com' },
+  { email: 'tomas.creativo88@gmail.com' },
+  { email: 'vale.ideaspro@gmail.com' },
+];
+
+export const category = [
+  { nameCategory: 'Refrescos' },
+  { nameCategory: 'Cerveza' },
+  { nameCategory: 'Snacks' },
+  { nameCategory: 'Energizantes' },
+  { nameCategory: 'Singani' },
+];
+
+export const products = [
+  {
+    nameProduct: 'Coca-Cola 2L',
+    imageProduct:
+      'https://farmacorp.com/cdn/shop/files/909699_1200x1200.jpg?v=1714434898',
+    price: 8,
+    stock: 60,
+  },
+  {
+    nameProduct: 'Pepsi 3L',
+    imageProduct: 'https://laoferta.cl/wp-content/images/17878-1.jpg',
+    price: 12,
+    stock: 36,
+  },
+  {
+    nameProduct: 'Salvietti Papaya 2L',
+    imageProduct:
+      'https://salvietti.com.bo/wp-content/uploads/2020/12/enano-quienes-somos.png',
+    price: 10,
+    stock: 36,
+  },
+  {
+    nameProduct: 'Paceña 620ml',
+    imageProduct:
+      'https://www.agavemicromercado.com/wp-content/uploads/6CBN-PACE-Cerv-620ml.jpg',
+    price: 15,
+    stock: 65,
+  },
+  {
+    nameProduct: 'Huari Negra 330ml',
+    imageProduct:
+      'https://www.fidalga.com/cdn/shop/products/9ad962b6-3d67-4ffa-b243-0720db397853_fca0cff1-87b5-47a2-82ff-9d58e387d973.png?v=1653345084',
+    price: 10,
+    stock: 60,
+  },
+  {
+    nameProduct: 'Corona 355ml',
+    imageProduct:
+      'https://www.fidalga.com/cdn/shop/products/0173769.jpg?v=1653343587',
+    price: 12.5,
+    stock: 30,
+  },
+  {
+    nameProduct: 'Maní salado',
+    imageProduct:
+      'https://jumboargentina.vtexassets.com/arquivos/ids/788483/Man-Mani-King-Salado-Sin-Piel-X100g-1-976844.jpg?v=638253790004330000',
+    price: 8,
+    stock: 24,
+  },
+  {
+    nameProduct: 'Papas fritas Lays clásicas',
+    imageProduct:
+      'https://icnorteb2c.vteximg.com.br/arquivos/ids/155917-600-600/Papas-Fritas-Clasicas-Lays-185-Gr-1-460.jpg?v=637345691513100000',
+    stock: 12,
+    price: 15,
+  },
+  {
+    nameProduct: 'Red Bull 250ml',
+    imageProduct:
+      'https://farmacorp.com/cdn/shop/files/564111_1200x1200.jpg?v=1714434386',
+    price: 15,
+    stock: 90,
+  },
+  {
+    nameProduct: 'Monster Energy 473ml',
+    imageProduct:
+      'https://amarket.com.bo/cdn/shop/files/070847028406_700x700.jpg?v=1712344275',
+    price: 20,
+    stock: 12,
+  },
+  {
+    nameProduct: 'Casa real ',
+    imageProduct:
+      'https://www.singanicasareal.com/wp-content/uploads/2024/01/Casa-Real-Pagina-Web-Banner-Etiqueta-Roja-710x1024-1.jpg',
+    price: 90,
+    stock: 12,
+  },
+];
+
+export const levels = [{ nameLevel: 'admin' }, { nameLevel: 'standar' }];
+
+export const listUsers = [
+  {
+    name: 'Patito',
+    lastName: 'Blade Truce',
+    email: 'nena@gmail.com',
+    carnet: '8954523',
+    phone: 75850236,
+    password: 'password123',
+    profilePicture:
+      'https://res.cloudinary.com/dqgcyonb9/image/upload/v1723172445/Ballet/qqapxmcvvceb2epa33mr.png',
+  },
+  {
+    name: 'Nicolas',
+    lastName: 'Choque Lopez',
+    email: 'nico@gmail.com',
+    carnet: '6548950',
+    phone: 75693120,
+    password: 'password123',
+    picture:
+      'https://res.cloudinary.com/dqgcyonb9/image/upload/v1729092230/Ballet/ume76hsu8xbingqqa38w.png',
+  },
+  {
+    name: 'Pipocas',
+    lastName: 'Altamirano Morales',
+    email: 'pipocas@gmail.com',
+    carnet: '9856324',
+    phone: 74123695,
+    password: 'password123',
+    picture: '',
+  },
+  {
+    name: 'Pelucas',
+    lastName: 'Mendez Zapana',
+    email: 'pelucas@gmail.com',
+    carnet: '9820036',
+    phone: 79613458,
+    password: 'password123',
+    picture:
+      'https://res.cloudinary.com/dqgcyonb9/image/upload/v1733339396/Ballet/dpcm66v7zsaexyxdusqu.png',
+  },
+];
