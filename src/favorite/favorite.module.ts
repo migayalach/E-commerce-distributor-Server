f7183eeb2dac8150cf7fed839b7934a6b52b4103
migayalach <ayalachavezmiguel@gmail.com>
2025-04-19 17:40:27 -0400

    exports favoriteService

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -12,7 +12,7 @@ import { Favorite, FavoriteSchema } from './schema/favorite.schema';
       { name: Favorite.name, schema: FavoriteSchema },
     ]),
   ],
-  exports: [],
+  exports: [FavoriteService],
   providers: [FavoriteResolver, FavoriteService],
 })
 export class FavoriteModule {}
