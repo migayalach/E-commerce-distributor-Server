55ab0c3ae54c7ee50d6469f3d1082b5d439b7424
migayalach <ayalachavezmiguel@gmail.com>
2025-04-19 23:21:12 -0400

    import modules and schemas

--- src/sign/sign.module.ts
+++ src/sign/sign.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SignService } from './sign.service';
 import { SignResolver } from './sign.resolver';
+import { UserModule } from 'src/user/user.module';
+import { LevelModule } from 'src/level/level.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/schema/user.schema';
 
 @Module({
+  imports: [
+    UserModule,
+    LevelModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [SignResolver, SignService],
 })
 export class SignModule {}
