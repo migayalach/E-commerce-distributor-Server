f4391c5703001420341cca549f948b0e58b23bd0
migayalach <ayalachavezmiguel@gmail.com>
2025-04-21 12:11:47 -0400

    middlewares

---
+++ src/middlewares/subscribre/subscribre.middleware.spec.ts
@@ -0,0 +1,7 @@
+import { SubscribreMiddleware } from './subscribre.middleware';
+
+describe('SubscribreMiddleware', () => {
+  it('should be defined', () => {
+    expect(new SubscribreMiddleware()).toBeDefined();
+  });
+});
