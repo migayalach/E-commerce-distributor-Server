59504a9e3ce282575a02c5fc687515a2bd1e720a
migayalach <ayalachavezmiguel@gmail.com>
2025-04-22 23:50:42 -0400

    config time to expires token

--- src/sign/sign.module.ts
+++ src/sign/sign.module.ts
@@ -5,13 +5,20 @@ import { UserModule } from 'src/user/user.module';
 import { LevelModule } from 'src/level/level.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/user/schema/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     UserModule,
     LevelModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2m' },
+    }),
   ],
+  exports: [SignService],
   providers: [SignResolver, SignService],
 })
 export class SignModule {}
