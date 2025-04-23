11b933c7f8e33618cb11b3def42e918fc6800237
migayalach <ayalachavezmiguel@gmail.com>
2025-04-22 23:45:48 -0400

    create guard to verify token

---
+++ src/sign/guard/auth.guard.guard.guard.spec.ts
@@ -0,0 +1,7 @@
+import { AuthGuardGuardGuard } from './auth.guard.guard.guard';
+
+describe('AuthGuardGuardGuard', () => {
+  it('should be defined', () => {
+    expect(new AuthGuardGuardGuard()).toBeDefined();
+  });
+});
---
+++ src/sign/guard/auth.guard.guard.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ApolloError } from 'apollo-server-express';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = context.getArgByIndex(2);
+    const request = ctx.req;
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new ApolloError('Unauthorized', 'UNAUTHORIZED');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      request['access'] = payload;
+    } catch {
+      throw new ApolloError('Unauthorized', 'UNAUTHORIZED');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
