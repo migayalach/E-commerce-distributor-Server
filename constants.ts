f24b4491a2c3826a25ac6e0641599811128ad7a0
migayalach <ayalachavezmiguel@gmail.com>
2025-04-24 02:52:06 -0400

    added two new constans for generate password 'dimention and include special characters'

--- constants.ts
+++ constants.ts
@@ -1,4 +1,8 @@
 export const elements = 20;
+export const urlcommercialLogo =
+  'https://res.cloudinary.com/dqgcyonb9/image/upload/v1745460558/Comercial-Chavez/ggba2d7mbpqoumwltudf.jpg';
 export const urlWithOutImage =
   'https://res.cloudinary.com/dqgcyonb9/image/upload/v1738902668/Ballet/cdppgavrmxtrptdo1mfq.png';
 export const saltOrRounds = 12;
+export const lengthPassword = 15;
+export const includeSpecialChars = true;
