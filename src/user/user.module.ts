b9aee0625a18b4174ab00a5c7b5ee177d3c762f9
migayalach <ayalachavezmiguel@gmail.com>
2025-04-24 03:01:09 -0400

    created new funtion for getAllUsersAdmin

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,10 +6,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
 import { Favorite, FavoriteSchema } from 'src/favorite/schema/favorite.schema';
 import { Cart, CartSchema } from 'src/cart/schema/cart.schema';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
   imports: [
     LevelModule,
+    EmailModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([
       { name: Favorite.name, schema: FavoriteSchema },
