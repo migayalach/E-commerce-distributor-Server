e5248fd1b192fdc7774f6a0bf4056405d5448fb7
migayalach <ayalachavezmiguel@gmail.com>
2025-05-28 16:14:10 -0400

    added new params with name context to token

--- src/sign/sign.resolver.ts
+++ src/sign/sign.resolver.ts
@@ -1,27 +1,35 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { SignService } from './sign.service';
 import { SignInDto, SignUpDto } from './dto/sign.dto';
 import { SignResponse } from './interface/responseSing.interface';
 import { SignData } from './interface/sign.interface';
+import { Response } from 'express';
+import { RequestWithCookies } from 'types/express/index.types';
 
 @Resolver('Sign')
 export class SignResolver {
   constructor(private readonly signService: SignService) {}
 
   @Mutation(() => SignResponse, { name: 'in' })
-  async signIn(@Args('dataSignIn') dataSignIn: SignInDto): Promise<SignData> {
-    return await this.signService.signIn(dataSignIn);
+  async signIn(
+    @Args('dataSignIn') dataSignIn: SignInDto,
+    @Context() context: { res: Response },
+  ): Promise<SignData> {
+    return await this.signService.signIn(dataSignIn, context);
   }
 
   @Mutation(() => SignResponse, { name: 'up' })
-  async signUp(@Args('dataSignUp') dataSignUp: SignUpDto): Promise<SignData> {
-    return await this.signService.signUp(dataSignUp);
+  async signUp(
+    @Args('dataSignUp') dataSignUp: SignUpDto,
+    @Context() context: { res: Response },
+  ): Promise<SignData> {
+    return await this.signService.signUp(dataSignUp, context);
   }
 
   @Mutation(() => String)
   async refreshToken(
-    @Args('refreshToken') refreshToken: string,
+    @Context() context: { req: RequestWithCookies },
   ): Promise<string> {
-    return await this.signService._refreshToken(refreshToken);
+    return await this.signService._refreshToken(context);
   }
 }
