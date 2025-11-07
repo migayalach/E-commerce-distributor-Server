b546a3e3c41008d496062eb1154a4bc4d55153c9
migayalach <ayalachavezmiguel@gmail.com>
2025-11-07 10:00:21 -0400

    destroy files

--- src/quealification/quealification.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QuealificationService } from './quealification.service';
-import { QuealificationResolver } from './quealification.resolver';
-
-@Module({
-  providers: [QuealificationResolver, QuealificationService],
-})
-export class QuealificationModule {}
--- src/quealification/quealification.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolver } from '@nestjs/graphql';
-import { QuealificationService } from './quealification.service';
-
-@Resolver('Quealification')
-export class QuealificationResolver {
-  constructor(private readonly quealificationService: QuealificationService) {}
-}
