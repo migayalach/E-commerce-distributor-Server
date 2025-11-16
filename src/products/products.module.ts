9e5e107126767ac7f4380819886d82f2ab2cdef7
migayalach <ayalachavezmiguel@gmail.com>
2025-11-16 11:59:24 -0400

    import feedback module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,12 +6,14 @@ import { Product, ProductSchema } from './schema/product.schema';
 import { CategoryModule } from 'src/category/category.module';
 import { EmailModule } from 'src/email/email.module';
 import { UserModule } from 'src/user/user.module';
+import { FeatbackModule } from 'src/featback/featback.module';
 
 @Module({
   imports: [
     CategoryModule,
     EmailModule,
     UserModule,
+    FeatbackModule,
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
   exports: [ProductsService],
