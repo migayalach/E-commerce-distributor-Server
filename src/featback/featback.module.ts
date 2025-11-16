5b17ae5fe3c1f55ba1ce13b2d592ce55dc8e8aa6
migayalach <ayalachavezmiguel@gmail.com>
2025-11-16 11:52:55 -0400

    befero update the module

--- src/featback/featback.module.ts
+++ src/featback/featback.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { FeatbackService } from './featback.service';
 import { FeatbackResolver } from './featback.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Featback, FeatbackSchema } from 'src/products/schema/featback.schema';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Featback.name, schema: FeatbackSchema },
+    ]),
+    // ProductsModule,
+  ],
+  exports: [FeatbackService],
   providers: [FeatbackResolver, FeatbackService],
 })
 export class FeatbackModule {}
