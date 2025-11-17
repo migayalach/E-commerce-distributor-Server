d3b8f731c48b235b93f64c1db232604beb7765b5
migayalach <ayalachavezmiguel@gmail.com>
2025-11-16 20:04:34 -0400

    import Product model

--- src/featback/featback.module.ts
+++ src/featback/featback.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { FeatbackService } from './featback.service';
 import { FeatbackResolver } from './featback.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Featback, FeatbackSchema } from 'src/products/schema/featback.schema';
 import { ProductsModule } from 'src/products/products.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Featback.name, schema: FeatbackSchema },
     ]),
-    // ProductsModule,
+    UserModule,
+    forwardRef(() => ProductsModule),
   ],
   exports: [FeatbackService],
   providers: [FeatbackResolver, FeatbackService],
