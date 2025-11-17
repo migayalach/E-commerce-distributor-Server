83d945573acc7784cd55765409c483542c5135bf
migayalach <ayalachavezmiguel@gmail.com>
2025-11-17 10:43:08 -0400

    creatred new mutarion set and reset qualification and query to get all qualification

--- src/qualification/qualification.resolver.ts
+++ src/qualification/qualification.resolver.ts
@@ -1,14 +1,41 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { QualificationService } from './qualification.service';
 import { ResponseInfo } from '@interface/response.interface';
+import { PagQualificationResponse } from './dto/pag-qualification-res.dto';
+import { AddQualificationDto } from './dto/create-qualification.dto';
+import { RespInfoBase } from '@interface/data.info.interface';
+import { DeleteQualificationDto } from './dto/delete-qualification.dto';
+import { Response } from '@interface/response.results.interface';
 
 @Resolver('Qualification')
 export class QualificationResolver {
   constructor(private readonly qualificationService: QualificationService) {}
 
   @Mutation(() => ResponseInfo)
-  addQualification() {}
+  async setQualification(
+    @Args('dataQualification') dataQualification: AddQualificationDto,
+  ): Promise<RespInfoBase> {
+    return await this.qualificationService.setQualification(dataQualification);
+  }
 
   @Mutation(() => ResponseInfo)
-  updateQualification() {}
+  async resetQualification(
+    @Args('dataQualification') dataQualification: DeleteQualificationDto,
+  ): Promise<RespInfoBase> {
+    return await this.qualificationService.resetQualification(
+      dataQualification,
+    );
+  }
+
+  @Query(() => PagQualificationResponse)
+  async getAllFeedback(
+    @Args('idQualification', { type: () => String, nullable: false })
+    idQualification: string,
+    @Args('page', { type: () => Int, nullable: true }) page: number = 1,
+  ): Promise<Response> {
+    return await this.qualificationService.getAllQualifications(
+      idQualification,
+      page,
+    );
+  }
 }
