676d77729400354f8bac093caf19a4224ff14527
migayalach <ayalachavezmiguel@gmail.com>
2025-11-17 08:39:03 -0400

    imports UserModule, ProductModule and Qualification

--- src/qualification/qualification.module.ts
+++ src/qualification/qualification.module.ts
@@ -1,8 +1,23 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { QualificationService } from './qualification.service';
 import { QualificationResolver } from './qualification.resolver';
+import { ProductsModule } from 'src/products/products.module';
+import { UserModule } from 'src/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Qualification,
+  QualificationSchema,
+} from 'src/products/schema/qualification.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Qualification.name, schema: QualificationSchema },
+    ]),
+    UserModule,
+    forwardRef(() => ProductsModule),
+  ],
+  exports: [QualificationService],
   providers: [QualificationResolver, QualificationService],
 })
 export class QualificationModule {}
