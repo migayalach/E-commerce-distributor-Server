d12dafeede85ae76bf0e0f1ab74e3210a71dc004
migayalach <ayalachavezmiguel@gmail.com>
2025-11-16 20:31:57 -0400

    import UserModule

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,9 +4,11 @@ import { CartResolver } from './cart.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, CartSchema } from './schema/cart.schema';
 import { ProductsModule } from 'src/products/products.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     ProductsModule,
     MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
   ],
