d14489b0c15e1e559a717b39d032de1a19aed87c
migayalach <ayalachavezmiguel@gmail.com>
2025-11-17 10:38:47 -0400

    import Feadback and Qualification module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsResolver } from './products.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,14 +7,16 @@ import { CategoryModule } from 'src/category/category.module';
 import { EmailModule } from 'src/email/email.module';
 import { UserModule } from 'src/user/user.module';
 import { FeatbackModule } from 'src/featback/featback.module';
+import { QualificationModule } from 'src/qualification/qualification.module';
 
 @Module({
   imports: [
     CategoryModule,
     EmailModule,
     UserModule,
-    FeatbackModule,
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    forwardRef(() => FeatbackModule),
+    forwardRef(() => QualificationModule),
   ],
   exports: [ProductsService],
   providers: [ProductsResolver, ProductsService],
