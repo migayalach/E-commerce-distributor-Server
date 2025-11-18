2c851cafd42380e577c9e637b0aa4b8029da9a46
migayalach <ayalachavezmiguel@gmail.com>
2025-11-18 11:44:26 -0400

    import userModule

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -4,9 +4,11 @@ import { FavoriteResolver } from './favorite.resolver';
 import { ProductsModule } from 'src/products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Favorite, FavoriteSchema } from './schema/favorite.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     ProductsModule,
     MongooseModule.forFeature([
       { name: Favorite.name, schema: FavoriteSchema },
