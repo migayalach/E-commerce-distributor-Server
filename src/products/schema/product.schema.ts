6a97008fad9b6dae09032433454b765a39748393
migayalach <ayalachavezmiguel@gmail.com>
2025-11-18 11:57:34 -0400

    added new properties 'idFeedback' and 'idQualifications'

--- src/products/schema/product.schema.ts
+++ src/products/schema/product.schema.ts
@@ -6,6 +6,12 @@ export class Product extends Document {
   @Prop({ type: Types.ObjectId, ref: 'Category', required: true })
   idCategory: Types.ObjectId;
 
+  @Prop({ type: Types.ObjectId, ref: 'Featback', required: true })
+  idFeatback: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'Qualification', required: true })
+  idQualification: Types.ObjectId;
+
   @Prop({ required: true })
   nameProduct: string;
 
@@ -17,6 +23,12 @@ export class Product extends Document {
 
   @Prop({ type: [String], required: true })
   imageProduct: string[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+  usersFavorite: Array<Types.ObjectId>;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+  usersCart: Array<Types.ObjectId>;
 
   @Prop({ default: true })
   state: boolean;
